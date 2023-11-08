6712e10576cf5e532171c4792b232f4888d75cb5
Laura Botero <lauraboterob@hotmail.es>
2023-11-08 10:53:37 +0100

    Update for accounting for the atmospheric attenuation

--- 1 - Code/Coordinates_definition.m
+++ 1 - Code/Coordinates_definition.m
@@ -1,10 +1,22 @@
-function  [y1, y2, y3] = Coordinates_definition(inputs,twist,AoA,Ra,Psi,Mbc0Te)
+function  [y1, y2, y3,dist] = Coordinates_definition(inputs,twist,AoA,Ra,Psi,Mbc0Te)
 
 %% observer coordinates - in the plane XZ
 X_obs = inputs.R0x;
 Y_obs = inputs.R0y;
 Z_obs = inputs.R0z;
-Angle = inputs.pitch+twist; % if you want to align with the flow and not the airfoil chord you should sum the AoA here. 
+Angle = inputs.pitch+twist;
+
+% %% move to the moving reference frame
+% X_obs = inputs.R0*sind(inputs.Theta);
+% Y_obs = 0;
+% Z_obs = inputs.R0*cosd(inputs.Theta);
+% x1_obs = inputs.R0*(sind(inputs.Theta)*cosd(Angle)*cosd(Psi)+inputs.R0*cosd(inputs.Theta)*sind(Angle));
+% x2_obs = -inputs.R0*sind(inputs.Theta)*sind(Psi);
+% x3_obs = inputs.R0*(-sind(inputs.Theta)*sind(Angle)*cosd(Psi)inputs.R0*cosd(inputs.Theta)*cosd(Angle));
+% %% Location of the midle of the Trailing edge 
+% y1 = x1_obs - 0;
+% y2 = x2_obs - Ra;
+% y3 = x3_obs - 0;
 Rr = [X_obs; Y_obs; Z_obs] - Mbc0Te(:);
 Mz = [cosd(Psi) sind(Psi) 0;...
     -sind(Psi) cosd(Psi) 0;...
@@ -21,6 +33,8 @@ Robs = Mx*(My*(Mz*Rr+d));
 y1 = Robs(1);
 y2 = Robs(2);
 y3 = Robs(3);
+
+dist = sqrt(y1^2+y2^2+y3^2);
 end
 
 
